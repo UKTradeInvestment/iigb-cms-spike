83ec82b0dd9424318699d8afcfd76cf224de3ae7
breneser <brhneser@gmail.com>
2016-08-31 14:59:22 +0100

    Changed prose folder location in server script

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', function(req, res) {
 
 app.set('case sensitive routing', false);
 
-app.use('/prose', express.static(__dirname + '/node_modules/prose'));
+app.use('/prose', express.static(__dirname + '/prose'));
 
 app.listen(port, function() {
     console.log('listening');
